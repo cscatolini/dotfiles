066a1cffe7e1f1777c19cf9388215fb8f4e70b0d
Camila Scatolini <camila.scatolini@gmail.com>
2018-04-02 16:08:31 -0300

    Fix insecure melpa.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.milkbox.net/packages/") t)
 
 (add-to-list 'load-path "~/.emacs.d/custom/")
 
